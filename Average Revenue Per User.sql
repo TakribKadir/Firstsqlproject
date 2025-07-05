a7ef8f38974db88e6e469606f18cd7fe916401cc
TakribKadir <mdtakribulkadir@gmail.com>
2025-07-04 21:42:23 -0600

    Average Revenue Per User

---
+++ Average Revenue Per User.sql
@@ -0,0 +1,11 @@
+--Average Revenue Per User
+WITH kpis AS ( 
+  SELECT 
+    SUM(meal_price * order_quantity) AS revenue, 
+    COUNT(DISTINCT user_id) AS users 
+  FROM meals 
+  JOIN orders ON meals.meal_id = orders.meal_id
+) 
+SELECT 
+  ROUND(revenue :: NUMERIC / GREATEST(users, 1), 2) AS arpu 
+FROM kpis;
